8f874d6847f1a519076187ba35131711cd8722ca
sumo <szkaczmarczyk@gmail.com>
2019-06-10 23:03:41 +0200

    spring setup

---
+++ src/main/java/com/kaczmarczyks/twotowers/TwoTowersApplication.java
@@ -0,0 +1,13 @@
+package com.kaczmarczyks.twotowers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoTowersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoTowersApplication.class, args);
+	}
+
+}
